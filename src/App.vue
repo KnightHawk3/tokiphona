fa18ac693e40e934a800e1270d7d332daca98257
Melody Kelly <melody@melody.blue>
2017-04-27 01:48:12 +1000

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div>Toki Pona is a language that in itself literally means "(language|to speak|spoken) (simple|good)"</br>
+    It has only the numbers, 1 (wan), 2 (tu), 5 (luka), 100 (mute). The words are always lowercase unless it is a non Toki Pona word, in which case it is uppercase.</br>
+
+    Please enter your phone number below in the format "My number is 451 million 234 thousand 567" but in Toki Pona of course.</br>
+    </div>
+    <hello></hello>
+    <span> Hint: "mi jo nunpa e" means "My phone number is" </span>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <input v-on:input="translate" v-model="message" placeholder="What is your phone number?"></input>
+    <p> Your phone number is: {{ number }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      number: 'invalid',
+      message: ''
+    }
+  },
+  methods: {
+    translate: function () {
+      if (this.message.indexOf('mi jo nunpa e') === 0) {
+        var count = 0
+        var message = this.message.split('mi jo nunpa e')[1].split(' ')
+        for (var i = 0; i < message.length; i++) {
+          if (message[i] === 'mute') {
+            count += 100
+          } else if (message[i] === 'luka') {
+            count += 5
+          } else if (message[i] === 'tu') {
+            count += 2
+          } else if (message[i] === 'wun') {
+            count += 1
+          }
+        }
+        this.number = count
+      } else {
+        this.number = 'invalid'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
